20195dd65e2fe0e530db73daffbd7b06a748c991
주진성 <jjsair0412@naver.com>
2021-11-17 21:34:54 +0900

    스레드로컬 완료

--- src/main/java/hello/advanced/LoGTraceConfig.java
+++ src/main/java/hello/advanced/LoGTraceConfig.java
@@ -1,15 +1,20 @@
 package hello.advanced;
 
-import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LoGTraceConfig {
 
+//    @Bean
+//    public LogTrace logTraceConfig(){
+//        return new FieldLogTrace();
+//    }
+
     @Bean
     public LogTrace logTraceConfig(){
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace(); // 스레드로컬로 변환
     }
 }
